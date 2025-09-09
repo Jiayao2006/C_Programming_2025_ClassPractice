b2a42ed2936e1d8e408df2e3db5d696e43335536
Jiayao2006 <148928521+Jiayao2006@users.noreply.github.com>
2025-09-09 10:48:03 +0900

    Add comments and update output in main.c
    Added explanatory comments to main.c for better understanding of the code and compilation process. Updated output statements to include a newline and an additional message. Recompiled main.exe files to reflect these changes.

--- Week 1/main.c
+++ Week 1/main.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
+#include <stdio.h> // brings in the declaration of printf
 
-int main(){
-    printf("Hello World!\n");
-    printf("Hello Ryan Peh");
+int main(){ // main function, returns int to the OS
+    printf("Hello World!\n"); // \n starts on a new line, 
+    printf("Hello Ryan Peh\n");
+    printf("Ryan is Gay");
     return 0;
 }
+
+// every line ends with ; just like css
+// remember to save the source file before compiling
+// gcc "Week 1/main.c" -o main  --> complies source into main.exe
+// run the executional file to run the programme
+
+// changes to this source file will require a recompiling to update the .exe
+// .c is human readable, .exe is machine code
+// common issue: forgot to include the first line, or not recompiling before running
